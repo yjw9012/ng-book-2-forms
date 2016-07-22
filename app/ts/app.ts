dec5e89426f914429e671e3398a7aaa9530edff7
Jiwoong Youn <yjw9012job@gmail.com>
2016-07-21 23:57:50 -0700

    Validate form using FormControl as an instance

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -20,17 +20,19 @@ require('../images/favicon.ico');
  */
 import {DemoFormSku} from './forms/demo_form_sku';
 import {DemoFormSkuBuilder} from './forms/demo_form_sku_with_builder';
+import {DemoFormWithValidationsExplicit} from './forms/demo_form_with_validations_explicit';
 
 /*
  * Webpack
  */
 @Component({
     selector: 'forms-demo-app',
-    directives: [DemoFormSku, DemoFormSkuBuilder],
+    directives: [DemoFormSku, DemoFormSkuBuilder, DemoFormWithValidationsExplicit],
     template: `
         <div>
             <demo-form-sku></demo-form-sku>
             <demo-form-sku-builder></demo-form-sku-builder>
+            <demo-form-with-validations-explicit></demo-form-with-validations-explicit>
         </div>
     `
 })
